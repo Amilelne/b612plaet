cade740da2d7e73ceee8f514fccb0faf9fd30520
joytzli <joytzli@tencent.com>
2024-06-14 20:20:21 +0800

    八卦

--- src/views/PlumView.vue
+++ src/views/PlumView.vue
@@ -3,6 +3,44 @@ import { ref } from 'vue';
 
 const num1 = ref();
 const num2 = ref();
+
+const BaGuaMap = {
+  0: {
+    num: 0,
+    name: '坤'
+  },
+  1: {
+    num: 4,
+    name: '震'
+  },
+  2: {
+    num: 3,
+    name: '离'
+  },
+  3: {
+    num: 2,
+    name: '兑'
+  },
+  4: {
+    num: 7,
+    name: '艮'
+  },
+  5: {
+    num: 6,
+    name: '坎'
+  },
+  6: {
+    num: 5,
+    name: '巽'
+  },
+  7: {
+    num: 7,
+    name: '乾'
+  }
+}
+
+const BaGua = ['坤', '乾', '兑', '离', '震', '巽', '坎', '艮'];
+
 function generatePlum() {
   console.log(num1.value, num2.value);
   if (validateNum()) {
@@ -10,7 +48,8 @@ function generatePlum() {
     const XiaGua = num2.value % 8;
     const BianYao = (num1.value + num2.value) % 6;
     console.log(ShangGua, XiaGua, BianYao);
-    showGua(ShangGua, XiaGua, BianYao)
+    showGua(ShangGua, XiaGua)
+    BianGua(ShangGua, XiaGua, BianYao);
   }
 }
 
@@ -22,10 +61,39 @@ function validateNum() {
   return false;
 }
 
-function showGua(ShangGua, XiaGua, BianYao) {
-  const BaGua = ['坤', '乾', '兑', '离', '震', '巽', '坎', '艮'];
-
+function showGua(ShangGua, XiaGua) {
   console.log(`上卦: ${BaGua[ShangGua]} 下卦：${BaGua[XiaGua]}`);
+}
+
+function BianGua(ShangGua, XiaGua, BianYao) {
+  let guaByte;
+  let bianGuaPartialByte;
+  if (BianYao >= 1 && BianYao <= 3) {
+    guaByte = getGuaByte(XiaGua);
+    bianGuaPartialByte = guaByte ^ (1 << (BianYao - 1));
+    const shangBianGua = BaGuaMap[bianGuaPartialByte].name;
+    console.log(`上卦: ${shangBianGua} 下卦：${BaGua[XiaGua]}`);
+  } else {
+    guaByte = getGuaByte(ShangGua);
+    if (BianYao === 0) {
+      bianGuaPartialByte = guaByte ^ (1 << 2);
+    } else {
+      bianGuaPartialByte = guaByte ^ (1 << (BianYao - 4));
+    }
+    const xiaBianGua = BaGuaMap[bianGuaPartialByte].name;
+    console.log(`上卦: ${BaGua[ShangGua]} 下卦：${xiaBianGua}`);
+  }
+}
+
+function getGuaByte(guaNum) {
+  let guaByte;
+  for (const [key, value] of Object.entries(BaGuaMap)) {
+    if (value.num === guaNum) {
+      guaByte = key;
+    }
+  }
+  console.log("xxx guaByte", guaByte);
+  return guaByte;
 }
 </script>
 
